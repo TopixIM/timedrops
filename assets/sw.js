86bada360c8c543fa720ac94168bda097ba17797
jiyinyiyong <jiyinyiyong@gmail.com>
2016-05-31 23:40:03 +0800

    network first for HTML

--- assets/sw.js
+++ assets/sw.js
@@ -1,13 +1,13 @@
 
 console.log('loaded sw.js');
 
-var CACHE_NAME = '0.0.1-1';
+var appVersion = '0.0.1-2';
 var urlsToCache = [];
 
 self.addEventListener('install', function(event) {
   // Perform install steps
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
+    caches.open(appVersion).then(function(cache) {
       console.log('Opened cache');
       return cache.addAll(urlsToCache);
     })
@@ -15,11 +15,12 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('activate', function (event) {
+  console.log('new version', appVersion);
   event.waitUntil(
     caches.keys().then(function (cacheNames) {
       return Promise.all(
         cacheNames.filter(function (cacheName) {
-          return cacheName !== CACHE_NAME
+          return cacheName !== appVersion
         }).map(function (cacheName) {
           console.log('delete cacheName', cacheName);
           return caches.delete(cacheName)
@@ -30,9 +31,19 @@ self.addEventListener('activate', function (event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+  // console.log('fetching:', event.request);
+  if (event.request.url.match(/\.html/)) {
+    event.respondWith(fetch(event.request).catch(function(error) {
+      console.log('no network', error);
+      caches.match(event.request).then(function(response) {
+        return response;
+      })
+    }));
+  } else {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  }
 });
